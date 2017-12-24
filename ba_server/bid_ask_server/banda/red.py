1d8ab755e8a85a9fecaa312f2787b065469268b2
Jonah246 <b03902085@ntu.edu.tw>
2017-12-24 16:17:09 +0800

    pair

--- ba_server/bid_ask_server/banda/red.py
+++ ba_server/bid_ask_server/banda/red.py
@@ -4,194 +4,199 @@ import os
 
 print ("============,", os.environ['REDIS_HOST'])
 rs = redis.Redis(host=os.environ['REDIS_HOST'], port=6379, db=0)
-rs.set("global_trade_id", 0)
+rs.set("defaultglobal_trade_id", 0)
 
 
 # get from redis and unpickle
 #unpacked_object = pickle.loads(rs.get('c'))
 
 
-def get_bid_by_id(userid, pop=True):
-    l = rs.lrange("bid_uid", 0, -1)
+def get_bid_by_id(userid, pop=True, pair = "default"):
+    l = rs.lrange(pair + "bid_uid", 0, -1)
     for i in l:
         if int(i) == userid:
-            price = rs.get("bid_price_%d" %(userid))
-            amount = rs.get("bid_amount_%d" %(userid))
-            timestamp = rs.get("bid_timestamp_%d" %(userid))
+            price = rs.get(pair + "bid_price_%d" %(userid))
+            amount = rs.get(pair + "bid_amount_%d" %(userid))
+            timestamp = rs.get(pair + "bid_timestamp_%d" %(userid))
             ## delete it
             if pop:
-                rs.lrem("bid_uid", i)
-                rs.delete("bid_price_%d" %(userid))
-                rs.delete("bid_amount_%d" %(userid))
-                rs.delete("bid_timestamp_%d" %(userid))
-            return Bid(price, amount, userid, timestamp)
+                rs.lrem(pair+ "bid_uid", i)
+                rs.delete(pair + "bid_price_%d" %(userid))
+                rs.delete(pair + "bid_amount_%d" %(userid))
+                rs.delete(pair + "bid_timestamp_%d" %(userid))
+            return Bid(price, amount, userid, timestamp, pair)
     return None
 
-def get_ask_by_id(userid, pop=True):
-    l = rs.lrange("ask_uid", 0, -1)
+def get_ask_by_id(userid, pop=True, pair = "default"):
+    l = rs.lrange(pair + "ask_uid", 0, -1)
     for i in l:
         if int(i) == userid:
-            price = rs.get("ask_price_%d" %(userid))
-            amount = rs.get("ask_amount_%d" %(userid))
-            timestamp = rs.get("ask_timestamp_%d" %(userid))
+            price = rs.get(pair + "ask_price_%d" %(userid))
+            amount = rs.get(pair + "ask_amount_%d" %(userid))
+            timestamp = rs.get(pair + "ask_timestamp_%d" %(userid))
             if pop:
                 ## delete it
-                rs.lrem("ask_uid", i)
-                rs.delete("ask_price_%d" %(userid))
-                rs.delete("ask_amount_%d" %(userid))
-                rs.delete("ask_timestamp_%d" %(userid))
-            return Ask(price, amount, userid, timestamp)
+                rs.lrem(pair + "ask_uid", i)
+                rs.delete(pair + "ask_price_%d" %(userid))
+                rs.delete(pair + "ask_amount_%d" %(userid))
+                rs.delete(pair + "ask_timestamp_%d" %(userid))
+            return Ask(price, amount, userid, timestamp, pair)
     return None
 
 
 class Trade(object):
-    def __init__(self, price, amount, id1, id2, trade_id):
+    def __init__(self, price, amount, id1, id2, trade_id, pair):
         self.price = int(price)
         self.amount = int(amount)
         self.trade_id = int(trade_id)
         self.id1 = int(id1)
         self.id2 = int(id2)
+        self.pair = pair
 
     def save_to_db(self):
-        rs.lpush("trade_id", self.trade_id)
-        rs.hset("trade_id1", self.trade_id, self.id1)
-        rs.hset("trade_id2", self.trade_id, self.id2)
-        rs.hset("trade_amount", self.trade_id, self.amount)
-        rs.hset("trade_price", self.trade_id, self.price)
+        rs.lpush(self.pair + "trade_id", self.trade_id)
+        rs.hset(self.pair + "trade_id1", self.trade_id, self.id1)
+        rs.hset(self.pair + "trade_id2", self.trade_id, self.id2)
+        rs.hset(self.pair + "trade_amount", self.trade_id, self.amount)
+        rs.hset(self.pair + "trade_price", self.trade_id, self.price)
 
     def to_dict():
         d = {}
         d["price"] =  self.price
         d["amount"] = self.amount
         d["timestamp"] = self.trade_id
+        d["pair"] = self.pair
         return d
 
 
 class Bid(object):
-    def __init__(self, price, amount, userid, timestamp):
+    def __init__(self, price, amount, userid, timestamp, pair = "default"):
         self.price = int(price)
         self.userid = int(userid)
         self.timestamp = int(timestamp)
         self.amount = int(amount)
+        self.pair = pair
 
     def save_to_db(self):
-        rs.lpush("bid_uid", self.userid)
-        rs.set("bid_price_%d" %(self.userid), self.price)
-        rs.set("bid_amount_%d" %(self.userid), self.amount)
-        rs.set("bid_timestamp_%d" %(self.userid), self.timestamp)
+        rs.lpush(self.pair + "bid_uid", self.userid)
+        rs.set(self.pair + "bid_price_%d" %(self.userid), self.price)
+        rs.set(self.pair + "bid_amount_%d" %(self.userid), self.amount)
+        rs.set(self.pair + "bid_timestamp_%d" %(self.userid), self.timestamp)
 
     def to_dict():
         d = {}
         d["price"] =  self.price
         d["amount"] = self.amount
+        d["pair"] = self.pair
         return d
 
 class Ask(object):
-    def __init__(self, price, amount, userid, timestamp):
+    def __init__(self, price, amount, userid, timestamp, pair = "default"):
         self.price = int(price)
         self.userid = int(userid)
         self.timestamp = int(timestamp)
         self.amount = int(amount)
+        self.pair = pair
 
     def save_to_db(self):
-        rs.lpush("ask_uid", self.userid)
-        rs.set("ask_price_%d" %(self.userid), self.price)
-        rs.set("ask_amount_%d" %(self.userid), self.amount)
-        rs.set("ask_timestamp_%d" %(self.userid), self.timestamp)
+        rs.lpush(self.pair + "ask_uid", self.userid)
+        rs.set(self.pair + "ask_price_%d" %(self.userid), self.price)
+        rs.set(self.pair + "ask_amount_%d" %(self.userid), self.amount)
+        rs.set(self.pair + "ask_timestamp_%d" %(self.userid), self.timestamp)
 
 
     def to_dict():
         d = {}
         d["price"] =  self.price
         d["amount"] = self.amount
+        d["pair"] = self.pair
         return d
 
 
-
-def new_bid(price, amount, userid, timestamp):
+def new_bid(price, amount, userid, timestamp, pair = "default"):
     bid = get_bid_by_id(int(userid))
     if bid != None:
         print("there's same tic exist")
-    bid = Bid(price, amount, userid, timestamp)
+    bid = Bid(price, amount, userid, timestamp, pair)
     if amount != 0 and price != 0:
         bid.save_to_db()
     else:
         print("rm ask", userid)
 
-def new_ask(price, amount, userid, timestamp):
+def new_ask(price, amount, userid, timestamp, pair = "default"):
     print("=====================new_ask===========\n", userid)
-    ask = get_ask_by_id(int(userid))
+    ask = get_ask_by_id(int(userid), pair)
     if ask != None:
         print("there's same tic exist")
-    ask = Ask(price, amount, int(userid), timestamp)
+    ask = Ask(price, amount, int(userid), timestamp, pair)
     if amount != 0 and price != 0:
         ask.save_to_db()
     else:
         print("rm ask", userid)
 
-def get_high_ask():
-    if rs.llen("ask_uid")==0:
+def get_high_ask(pair = "default"):
+    if rs.llen(pair + "ask_uid")==0:
         return None
-    unpacked_object = get_ask_by_id(int(rs.sort("ask_uid", by='ask_price_*', desc=True)[0]), False)
+    unpacked_object = get_ask_by_id(int(rs.sort(pair + "ask_uid", by=pair + 'ask_price_*', desc=True)[0]), False, pair)
     return unpacked_object
 
-def get_low_bid():
-    if rs.llen("bid_uid")==0:
+def get_low_bid(pair = "default"):
+    if rs.llen(pair + "bid_uid")==0:
         return None
 
-    unpacked_object = get_bid_by_id(int(rs.sort("bid_uid", by='bid_price_*')[0]), False)
+    unpacked_object = get_bid_by_id(int(rs.sort(pair + "bid_uid", by=pair + 'bid_price_*')[0]), False, pair)
     return unpacked_object
 
-def get_asks(count):
+def get_asks(count, pair = "default"):
     r = []
     #if rs.llen("ask_uid")<count:
     #    return False
-    l = rs.sort("ask_uid", by='ask_price_*', desc=True)[0:count]
+    l = rs.sort(pair + "ask_uid", by=pair + 'ask_price_*', desc=True)[0:count]
     for i in l:
-        unpacked_object = get_ask_by_id(int(i), False)
+        unpacked_object = get_ask_by_id(int(i), False, pair)
         r.append(unpacked_object)
     return r
 
-def get_bids(count):
+def get_bids(count, pair = "default"):
     r = []
-    l = rs.sort("bid_uid", by='bid_price_*')[0:count]
+    l = rs.sort(pair + "bid_uid", by=pair + 'bid_price_*')[0:count]
     for i in l:
-        unpacked_object = get_bid_by_id(int(i), False)
+        unpacked_object = get_bid_by_id(int(i), False, pair)
         r.append(unpacked_object)
     return r
 
-def new_deal(price, amount, id1, id2, trade_id):
-    deal = Trade(price, amount, id1, id2, trade_id)
+def new_deal(price, amount, id1, id2, trade_id, pair = "default"):
+    deal = Trade(price, amount, id1, id2, trade_id, pair)
     deal.save_to_db()
-    print(price, amount, id1, id2)
+    print(price, amount, id1, id2, pair)
     return True
 
-def get_deals(num):
+def get_deals(num, pair = "default"):
     r = []
-    l = rs.lrange("trade_id", 0, num-1)
+    l = rs.lrange(pair + "trade_id", 0, num-1)
     for i in l:
-        id1 = rs.hget("trade_id1", int(i))
-        id2 = rs.hget("trade_id2", int(i))
-        amount = rs.hget("trade_amount", int(i))
-        price = rs.hget("trade_price", int(i))
-        r.append(Trade(price, amount, id1, id2, int(i)))
+        id1 = rs.hget(pair + "trade_id1", int(i))
+        id2 = rs.hget(pair + "trade_id2", int(i))
+        amount = rs.hget(pair + "trade_amount", int(i))
+        price = rs.hget(pair + "trade_price", int(i))
+        r.append(Trade(price, amount, id1, id2, int(i), pair))
     return r
 
-def match():
+def match(pair = "default"):
     try:
-        ask = get_high_ask()
-        bid = get_low_bid()
+        ask = get_high_ask(pair)
+        bid = get_low_bid(pair)
         if ask.price >= bid.price:
             deal_price = bid.price
             if ask.amount > bid.amount:
                 deal_amount = int(bid.amount)
             else:
                 deal_amount = int(ask.amount)
-            if new_deal(deal_price, deal_amount, ask.userid, bid.userid, int(rs.get("global_trade_id"))):
-                rs.set("global_trade_id", int(rs.get("global_trade_id")) + 1)
+            if new_deal(deal_price, deal_amount, ask.userid, bid.userid, int(rs.get("global_trade_id")), pair):
+                rs.set(pair + "global_trade_id", int(rs.get(pair + "global_trade_id")) + 1)
 
-            new_bid(bid.price, int(bid.amount)-deal_amount, bid.userid, bid.timestamp)
-            new_ask(ask.price, int(ask.amount)-deal_amount, ask.userid, ask.timestamp)
+            new_bid(bid.price, int(bid.amount)-deal_amount, bid.userid, bid.timestamp, pair)
+            new_ask(ask.price, int(ask.amount)-deal_amount, ask.userid, ask.timestamp, pair)
             ## match
             return True
         return False
